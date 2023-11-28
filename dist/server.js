b6d0937d0e711d65f1af3a2db98ae9b49bb04474
JVFeijo <joaovictor.feijo12@gmail.com>
2023-11-28 17:13:48 -0300

    add routes for all basic methods

--- dist/server.js
+++ dist/server.js
@@ -25,9 +25,21 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = __importStar(require("http"));
 const server = http.createServer((req, res) => {
+    if (req.url === '/api' && req.method === 'GET') {
+        res.writeHead(200);
+        return res.end(JSON.stringify(`route: ${req.url}, method: GET`));
+    }
     if (req.url === '/api' && req.method === 'POST') {
         res.writeHead(200);
         return res.end(JSON.stringify(`route: ${req.url}, method: POST`));
+    }
+    if (req.url === '/api' && req.method === 'PUT') {
+        res.writeHead(200);
+        return res.end(JSON.stringify(`route: ${req.url}, method: PUT`));
+    }
+    if (req.url === '/api' && req.method === 'DELETE') {
+        res.writeHead(200);
+        return res.end(JSON.stringify(`route: ${req.url}, method: DELETE`));
     }
     res.writeHead(200);
     return res.end(JSON.stringify('default'));
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import * as http from 'http'
 
 const server = http.createServer((req, res) => {
+    if (req.url === '/api' && req.method === 'GET') {
+        res.writeHead(200)
+        return res.end(JSON.stringify(`route: ${req.url}, method: GET`))
+    }
     if (req.url === '/api' && req.method === 'POST') {
         res.writeHead(200)
         return res.end(JSON.stringify(`route: ${req.url}, method: POST`))
+    }
+    if (req.url === '/api' && req.method === 'PUT') {
+        res.writeHead(200)
+        return res.end(JSON.stringify(`route: ${req.url}, method: PUT`))
+    }
+    if (req.url === '/api' && req.method === 'DELETE') {
+        res.writeHead(200)
+        return res.end(JSON.stringify(`route: ${req.url}, method: DELETE`))
     }
     res.writeHead(200)
     return res.end(JSON.stringify('default'))
